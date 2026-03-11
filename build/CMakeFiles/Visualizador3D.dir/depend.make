c58e71eeaf2997271d1b22057193e39b53f3a766
Yordan Vasquez <vyordangiovani@gmail.com>
2026-03-11 17:21:41 -0600

    git estaba trackeando archivos compilados de la carpeta buil igual que de la carpeta .vscode, ahora ya no lo hace y en teorida tendria que funcionar el listado a ignorar en el .gitignore

--- build/CMakeFiles/Visualizador3D.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Visualizador3D.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/Visualizador3D.dir/src/Conexion.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Visualizador3D.dir/src/Conexion.cpp.o: \
- /home/yordan/Documentos/C++/visualizador_3d/src/Conexion.cpp \
- /usr/include/stdc-predef.h \
- /home/yordan/Documentos/C++/visualizador_3d/src/Conexion.h \
- /home/yordan/Documentos/C++/visualizador_3d/src/Matematicas/Vector3.h
